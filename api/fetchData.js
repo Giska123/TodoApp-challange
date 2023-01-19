35a273648c10bef9126e2359ce45ed8c9fce7282
Giska123 <giskahalla123@gmail.com>
2023-01-19 12:00:13 +0700

    modified file

--- api/fetchData.js
+++ api/fetchData.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 export default async (req, res) => {
-    const data = await fetch(`http://localhost:${process.env.JSON_SERVER_PORT}/activity`)
+    const data = await fetch(`http://localhost:8004/activity`)
     const json = await data.json()
     res.json(json);
 }
